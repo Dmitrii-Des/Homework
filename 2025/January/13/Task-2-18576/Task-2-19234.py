302ebaf437d4701abac98e7f2491c0aee0aea698
Dmitrii <mr_dmitrii@mail.ru>
2025-01-22 19:08:18 +0200

    https://kompege.ru/variant?kim=25075893 Task 2 completed(1 method)

--- 2025/January/13/Task-2-18576/Task-2-19234.py
+++ 2025/January/13/Task-2-18576/Task-2-19234.py
@@ -1,7 +1,7 @@
 from itertools import permutations, product
 def f(x, y, z, w):
-    return(((not(((not x )or y) and (not w))) or (not(z and (not(y and w))))))
-   for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    return((not(((not x )or y) and (not w))) or (not(z and (not(y and w)))))
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
     table = [(0, a1, a2, 1), (a3, 1, a4, a5), (1, 0, a6, a7)]
     if len(table) == len(set(table)):
         for p in permutations('xyzw'):
