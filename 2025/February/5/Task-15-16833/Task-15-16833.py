f47ec5bd42cfc419d9ec15b09b9c296fbdcd1e74
Dmitrii <mr_dmitrii@mail.ru>
2025-02-05 22:15:27 +0200

    https://kompege.ru/variant?kim=25080370 Task 15 completed

---
+++ 2025/February/5/Task-15-16833/Task-15-16833.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+def f(x):
+    P = 25 <= x <= 73
+    Q = 75 <= x <= 118
+    A = A1 <= x <= A2
+    return (A and (not Q)) <= (P or Q)
+ans = []
+line = [x/10 for x in range(24*10, 119*10)]
+for A1, A2 in combinations(line, 2):
+    if all(f(x) for x in line):
+        ans.append(A2-A1)
+print(max(ans))
