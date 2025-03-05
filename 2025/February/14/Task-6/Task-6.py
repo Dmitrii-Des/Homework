b0526981aa270f80e722f5e1b7e1e9b49a8c2475
Dmitrii <mr_dmitrii@mail.ru>
2025-03-05 21:56:10 +0200

    https://inf-ege.sdamgia.ru/test?id=15469830 Task 6 completed

---
+++ 2025/February/14/Task-6/Task-6.py
@@ -0,0 +1,24 @@
+from turtle import*
+lt(90)
+tracer(0)
+m = 10
+
+up()
+for i in range(10):
+    rt(120)
+    fd(10 * m)
+down()
+for i in range(7):
+    fd(15 * m)
+    rt(90)
+for i in range(5):
+    rt(60)
+    fd(20 * m)
+    rt(30)
+up()
+for x in range(-20, 20):
+    for y in range(-30, 20):
+        goto(x * m, y * m)
+        dot(3, 'magenta')
+update()
+done()
