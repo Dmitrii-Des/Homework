216de724e10e650712d7fb9c374eddaba7c9e69b
Dmitrii <mr_dmitrii@mail.ru>
2025-03-31 15:24:20 +0200

    https://kompege.ru/variant?kim=25087422 Task 23 completed

---
+++ 2025/March/13/Task-23-13972/Task-23-13972.py
@@ -0,0 +1,10 @@
+def f(cur, end):
+    if cur == end:
+        return 1
+    if cur > end:
+        return 0
+    if cur == 7:
+        return f(cur + 2, end) + f(cur * 2, end)
+    return f(cur + 2, end) + f(cur + 5, end) + f(cur * 2, end)
+
+print(f(7, 23)*f(23, 35))
